f74ae5dff662af9ae95daa7f16a572d1822644b4
TheRealYT <85513618+TheRealYT@users.noreply.github.com>
2025-07-29 23:48:19 +0300

    add security standard for rejection

--- middlewares.js
+++ middlewares.js
@@ -1,6 +1,8 @@
 const {verifyAttestation} = require('./index');
 const {logger} = require('./logger');
 
+const validHashes = process.env.APP_HASHES.split('|');
+
 async function protect(req, res, next) {
   const challenge = req.headers['x-challenge'];
   const signature = req.headers['x-signature'];
@@ -17,9 +19,22 @@ async function protect(req, res, next) {
 
   await verifyAttestation(certChain, challenge, signature)
     .then(result => {
-      logger.info('Passed', {uuid, ...result});
+      logger.info('Valid', {uuid, ...result});
 
-      next();
+      if (
+        result.deviceLocked === true &&
+        result.bootState === 'Verified' &&
+        (result.implSecLevel === 'TrustedEnvironment' || result.implSecLevel === 'StrongBox') &&
+        (result.storeSecLevel === 'TrustedEnvironment' || result.storeSecLevel === 'StrongBox') &&
+        result.authType === 'Fingerprint' &&
+        result.packageName === process.env.APP_PKG_NAME &&
+        !result.appSigns.some(s => !validHashes.includes(s))
+      ) {
+        return next();
+      }
+
+      res.status(403).send('Access denied!');
+      return next(new Error('Device below the standard'));
     })
     .catch(err => {
       res.status(403).send('Access denied!');
